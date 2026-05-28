f1788fa10b8bfb95ee6b178138489e7a48a8efea
matajigahua-del <matajigahua@gmail.com>
2026-05-28 15:46:31 +0530

    first commit

---
+++ activity1.py
@@ -0,0 +1,54 @@
+#the '+' sign is used to add the values of two variables
+a=10
+b=888
+sum=a+b
+print(sum)
+
+#print in python
+print("hello world")
+
+#algorithmic calculation in print statement
+print(16+90,"This is the sum of 16 and 90")
+print(14-13,"This the difference of 14 and 13")
+print(12*45,"This is the product of of 12 and 45")
+print(25/5,"This the quotient of 25 and 5")
+
+#Indentifers in python- An Identifer is a name used to identify a variable,function,class,module etc., so it can be alphabet, a combination og alphabet, underscore and  digit.
+# Rules for naming an identifier in python
+#1. An identifier can only contain letters, digits and underscores. example: myname, name_1 and name_is
+# 2 An identifier cannot start with a digit. example: 1name, 2name and 3name are invalid identifiers, but class1.
+# 3. Kewords cannot be used as identifiers.
+# 4. We cannot use special characters like @, #, $, % etc., in an identifier. example: name@, name# and name$ are invalid identifiers.
+
+#Keywords in python- A keyword is a reserved word that has a special meaning in python and cannot be used as an identifier. example: if, else, for, while, def, class etc., are keywords in python.
+
+#Variables in python- A variable is a container that holds a value. It is used to store data in a program. example: name, age, salary etc., are variables in python.
+a="python"
+print(a,"this variable contains string value")
+
+c=10
+print(c,"this variable contains integer value")
+d=3.67
+print(d,"this variable contains float value")
+
+e=True
+print(e,"this is a boolean variable")
+
+#Self-introdduction in Python
+name="Kashvee"
+print("My name is",name)
+grade="9th grade"
+print("I am in grade",grade)
+school="Sun Valley International School"
+print("I go to a school called",school)
+hobby="Playing badminton and reading novels"
+print("My hobbies are",hobby)
+favourite_subject="Mathematics"
+print("My favourite subject is",favourite_subject)
+
+a="Good afternoon.I am Kashvee and I am in 9th grade. I go to a school called Sun Valley International School. My hobbies include playing badminton and reading novels. My favourite subject is Mathematics. I am here to pursue my passion of coding,python and to learn new things."
+print(a)
+
+
+
+
